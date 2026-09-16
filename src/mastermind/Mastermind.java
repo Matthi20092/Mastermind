1ef3663f7460ca1728208c3d263fddc64b73ed14
Matthi20092 <matthias.worbts@gmail.com>
2026-09-16 11:17:55 +0200

    New Strings
    New strings for the different colors

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -4,6 +4,20 @@ public class Mastermind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Mastermind Test");
+		String GeelPin="Geel";
+		String GroenPin="Groen";
+		String BlauwPin="Blauw";
+		String RoodPin="Rood";
+		String PaarsPin="Paars";
+		String OranjePin="Oranje";
+		
+		String ZwartPin="Zwart";
+		String WitPin="Wit";
+		String LeegPin="Leeg";
+
+
+
 
 	}
 
