abe4ee29eb3b7a102192dae5058674cce5c82529
Matthi20092 <matthias.worbts@gmail.com>
2026-09-18 15:27:13 +0200

    Update Mastermind.java

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -5,17 +5,18 @@ public class Mastermind {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mastermind Test");
-		String GeelPin="Geel";
-		String GroenPin="Groen";
-		String BlauwPin="Blauw";
-		String RoodPin="Rood";
-		String PaarsPin="Paars";
-		String OranjePin="Oranje";
+		String geelPin="Geel";
+		String groenPin="Groen";
+		String blauwPin="Blauw";
+		String roodPin="Rood";
+		String paarsPin="Paars";
+		String oranjePin="Oranje";
 		
-		String ZwartPin="Zwart";
-		String WitPin="Wit";
-		String LeegPin="Leeg";
-
+		String zwartPin="Zwart";
+		String witPin="Wit";
+		String leegPin="Leeg";
+		System.out.println("Speler 1 = Codemaker");
+		System.out.println("Speler 2 = Codekraker");
 
 
 
